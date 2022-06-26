1e078e510e0f1b52906d1dd28f7f7865926e0bcd
Sergey Shishkin <shishkinsergeydmitrievich@gmail.com>
2022-06-26 17:06:08 +0300

    Adds basic installer for GitBash terminals

---
+++ installer.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function install_for_gitbash {
+  if [ -n "$HOME" ]; then
+    # GitBash terminal case
+
+    # installing vuh
+    mkdir -p "$HOME/bin" || exit 1
+    cp -f vuh.bash "$HOME/bin/vuh" || exit 1
+
+    # installing autocompletion script
+    mkdir -p "$HOME/bash_completion.d" || exit 1
+    cp -f vuh-completion.bash "$HOME/bash_completion.d/vuh-completion.bash" || exit 1
+
+    # show success message
+    printf '\n'
+    echo 'vuh successfully installed'
+    echo 'Autocompletion will be available only after restarting your terminal!'
+  else
+    echo "HOME variable not found! aborting"
+    exit 1
+  fi
+}
+
+install_for_gitbash
