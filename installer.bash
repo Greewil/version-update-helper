ef3935cd57a4d0e73f46c32252df7fd66685061a
Sergey Shishkin <shishkinsergeydmitrievich@gmail.com>
2022-06-27 17:01:35 +0300

    Adds request for autorestarting bash terminal session after installation

--- installer.bash
+++ installer.bash
@@ -4,18 +4,33 @@ function install_for_gitbash {
   if [ -n "$HOME" ]; then
     # GitBash terminal case
 
-    # installing vuh
+    # install vuh
     mkdir -p "$HOME/bin" || exit 1
     cp -f vuh.bash "$HOME/bin/vuh" || exit 1
 
-    # installing autocompletion script
+    # install autocompletion script
     mkdir -p "$HOME/bash_completion.d" || exit 1
     cp -f vuh-completion.bash "$HOME/bash_completion.d/vuh-completion.bash" || exit 1
 
     # show success message
     printf '\n'
     echo 'vuh was successfully installed'
-    echo 'Autocompletion will be available only after restarting your terminal!'
+
+    # advice to restart current bash terminal session
+    asking_question='true'
+    while [ $asking_question = 'true' ]; do
+      read -p "To activate vuh-completion your should restart bash terminal session. Do you want to restart it now? (Y/N): " -r answer
+      case "$answer" in
+      y|Y|Yes|yes)
+        echo 'restarting current bash terminal session ...'
+        exec bash -l || exit 1
+        ;;
+      n|N|No|no)
+        echo 'Autocompletion will be available only after restarting your terminal!'
+        exit 0
+        ;;
+      esac
+    done
   else
     echo "HOME variable not found! aborting"
     exit 1
