b6e11a042d5aa9bf342282f759581b3915d04f76
Sergey Shishkin <shishkinsergeydmitrievich@gmail.com>
2022-08-12 00:15:38 +0300

    feat(installer): create data directory
    Create data dir due installation

--- installer.sh
+++ installer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Installer configuration
+INSTALLING_APP_NAME='vuh'
+
 # Output colors
 APP_NAME='vuh_installer'
 NEUTRAL_COLOR='\e[0m'
@@ -88,37 +91,54 @@ function _get_input_with_check() {
   done
 }
 
+function _get_script_source_path() {
+  source_path=${BASH_SOURCE[0]}
+  while [ -L "$source_path" ]; do # resolve $source_path until the file is no longer a symlink
+    output=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    source_path=$(readlink "$source_path")
+    [[ $source_path != /* ]] && source_path=$output/$source_path # if $source_path was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  output=$( cd -P "$( dirname "$source_path" )" >/dev/null 2>&1 && pwd )
+  echo "$output"
+}
+
 function _warning_should_restart() {
   _show_warning_message 'Autocompletion will be available only after restarting your shell session!'
 }
 
-function _check_vuh_version() {
-  installed_vuh_version=$(vuh -v) || return 1
-  available_vuh_version=$(./vuh.sh -v) || return 1
-  if [ "$installed_vuh_version" = "$available_vuh_version" ]; then
-    _show_updated_message 'Vuh was successfully installed'
+function _check_installed_version() {
+  # get installer path in case if installer started not from the same directory
+  installer_path=$(_get_script_source_path) || return 1
+  installed_version=$($INSTALLING_APP_NAME -v) || return 1
+  available_version=$("$installer_path/vuh.sh" -v) || return 1
+  if [ "$installed_version" = "$available_version" ]; then
+    _show_updated_message "$INSTALLING_APP_NAME was successfully installed"
   else
-    _show_error_message "Vuh version check failed! Something went wrong ..."
-    _show_warning_message "You still have an old version ($installed_vuh_version) instead of new ($available_vuh_version)!"
+    _show_error_message "$INSTALLING_APP_NAME version check failed! Something went wrong ..."
+    _show_warning_message "You still have an old version ($installed_version) instead of new ($available_version)!"
     return 1
   fi
 }
 
 function _install() {
+  # get installer path in case if installer started not from the same directory
+  installer_path=$(_get_script_source_path)
+
   # check PATH variable for INSTALLATION_DIR path
-  if [ "$(echo "$PATH" | grep "$INSTALLATION_DIR")" = '' ]; then
-    _show_error_message "Your won't be able to use vuh after installing it in $INSTALLATION_DIR!"
+  if [ "$(echo "$PATH" | grep ":$INSTALLATION_DIR:")" = '' ]; then
+    _show_error_message "($INSTALLATION_DIR not found in PATH)"
+    _show_error_message "Your won't be able to use $INSTALLING_APP_NAME from any place!"
     _show_error_message "Make sure that $INSTALLATION_DIR is in your PATH variable and launch installer.sh again!"
-    return 1
+    _yes_no_question "Do you still want to install $INSTALLING_APP_NAME in $INSTALLATION_DIR?" "" "return 1" || return 1
   fi
 
-  # install vuh
+  # install application
   mkdir -p "$INSTALLATION_DIR" || return 1
-  cp -f vuh.sh "$INSTALLATION_DIR/vuh" || return 1
+  cp -f "$installer_path/vuh.sh" "$INSTALLATION_DIR/$INSTALLING_APP_NAME" || return 1
 
   # install autocompletion script
   mkdir -p "$COMPLETION_DIR" || return 1
-  cp -f vuh-completion.sh "$COMPLETION_DIR/$COMPLETION_SCRIPT_NAME" || return 1
+  cp -f "$installer_path/vuh-completion.sh" "$COMPLETION_DIR/$COMPLETION_SCRIPT_NAME" || return 1
 
   # create data dir and configuration files
   mkdir -p "$DATA_DIR" || return 1
@@ -137,16 +157,16 @@ function _install() {
   touch "$DATA_DIR/latest_update_check"
   chmod 666 "$DATA_DIR/latest_update_check"
 
-  # update DATA_DIR variable in vuh
-  installed_vuh_script=$(<"$INSTALLATION_DIR/vuh") || {
-    _show_error_message "Failed to get file $INSTALLATION_DIR/vuh!"
+  # update DATA_DIR variable in application
+  installed_script=$(<"$INSTALLATION_DIR/$INSTALLING_APP_NAME") || {
+    _show_error_message "Failed to get file $INSTALLATION_DIR/$INSTALLING_APP_NAME!"
     return 1
   }
   string_to_replace='<should_be_replace_after_installation:DATA_DIR>'
-  echo "${installed_vuh_script/$string_to_replace/$DATA_DIR}" > "$INSTALLATION_DIR/vuh" || return 1
+  echo "${installed_script/$string_to_replace/$DATA_DIR}" > "$INSTALLATION_DIR/$INSTALLING_APP_NAME" || return 1
 
-  # check is vuh installed properly
-  _check_vuh_version || return 1
+  # check is application installed properly
+  _check_installed_version || return 1
 
   # warning to restart completion script
   _warning_should_restart || return 1
@@ -161,8 +181,8 @@ function _is_dir_exists() {
 
 function _is_installation_dir_ok() {
   dir=$1
-  _is_dir_exists $dir || return 1
-  path_grep=$(echo $PATH | egrep "$dir")
+  _is_dir_exists "$dir" || return 1
+  path_grep=$(echo "$PATH" | grep -E "$dir")
   if [ "$path_grep" = '' ]; then
     return 1
   fi
@@ -171,8 +191,8 @@ function _is_installation_dir_ok() {
 function _manual_select_installation_dir() {
   _show_function_title "select installation directory"
   recommended_dir="$INSTALLATION_DIR"
-#  ask_input_message="Enter directory path where you want to install vuh (recommended: $recommended_dir)" TODO add recommended dir
-  ask_input_message="Enter directory path where you want to install vuh"
+#  ask_input_message="Enter directory path where you want to install $INSTALLING_APP_NAME (recommended: $recommended_dir)" TODO add recommended dir
+  ask_input_message="Enter directory path where you want to install $INSTALLING_APP_NAME"
   output_variable_name="INSTALLATION_DIR"
   check_function="_is_installation_dir_ok" # here should be PATH check
   _warning="Directory either doesn't exist or wasn't specified in PATH!"
@@ -184,28 +204,28 @@ function _manual_select_installation_dir() {
 
 function _manual_select_completion_dir() {
   _show_function_title "select directory for autocompletion script"
-  recommended_dir="$COMPLETION_DIR"
-#  ask_input_message="Enter directory path where you want to install autocompletion script for vuh (recommended: $recommended_dir)" TODO add recommended dir
-  ask_input_message="Enter directory path where you want to install autocompletion script for vuh"
+#  recommended_dir="$COMPLETION_DIR"
+#  ask_input_message="Enter directory path where you want to install autocompletion script for $INSTALLING_APP_NAME (recommended: $recommended_dir)" TODO add recommended dir
+  ask_input_message="Enter directory path where you want to install autocompletion script for $INSTALLING_APP_NAME"
   output_variable_name="COMPLETION_DIR"
   check_function="_is_dir_exists"
   check_failed_message="Directory doesn't exist! Please select another directory or create this one!"
   _get_input_with_check "$ask_input_message" "$output_variable_name" "$check_function" "$check_failed_message"
   echo "directory for autocompletion script selected: $COMPLETION_DIR"
-  COMPLETION_SCRIPT_NAME='vuh'
-#  _get # TODO check if user want COMPLETION_SCRIPT_NAME to be vuh or vuh-completion.bash
+  COMPLETION_SCRIPT_NAME="$INSTALLING_APP_NAME"
+#  _get # TODO check if user want COMPLETION_SCRIPT_NAME to be $INSTALLING_APP_NAME or $INSTALLING_APP_NAME-completion.bash
 }
 
 function _manual_select_data_dir() {
   _show_function_title "select directory where script should store its configuration and other temporary data"
-  recommended_dir="$DATA_DIR"
-#  ask_input_message="Enter directory path where you want to store information about vuh script (recommended: $recommended_dir)" TODO add recommended dir
-  ask_input_message="Enter directory path where you want to store information about vuh script"
+#  recommended_dir="$DATA_DIR"
+#  ask_input_message="Enter directory path where you want to store information about $INSTALLING_APP_NAME script (recommended: $recommended_dir)" TODO add recommended dir
+  ask_input_message="Enter directory path where you want to store information about $INSTALLING_APP_NAME script"
   output_variable_name="DATA_DIR"
   check_function="_is_dir_exists"
   check_failed_message="Directory doesn't exist! Please select another directory or create this one!"
   _get_input_with_check "$ask_input_message" "$output_variable_name" "$check_function" "$check_failed_message"
-  DATA_DIR="$DATA_DIR/vuh"
+  DATA_DIR="$DATA_DIR/$INSTALLING_APP_NAME"
   echo "configuration directory for autocompletion script selected: $DATA_DIR"
 }
 
@@ -226,8 +246,8 @@ function _install_msys() {
     _show_function_title "Installing for msys ..."
     INSTALLATION_DIR="$HOME/bin"
     COMPLETION_DIR="$HOME/bash_completion.d"
-    COMPLETION_SCRIPT_NAME="vuh-completion.bash"
-    DATA_DIR="$HOME/bin/vuh-data"
+    COMPLETION_SCRIPT_NAME="$INSTALLING_APP_NAME-completion.bash"
+    DATA_DIR="$HOME/bin/$INSTALLING_APP_NAME-data"
     _install || return 1
   else
     _show_error_message "HOME variable not found! aborting"
@@ -240,13 +260,13 @@ function _install_unix_like() {
   INSTALLATION_DIR="/usr/bin"
   if [ -d /usr/share/bash-completion/completions ]; then
     COMPLETION_DIR="/usr/share/bash-completion/completions"
-    COMPLETION_SCRIPT_NAME="vuh"
+    COMPLETION_SCRIPT_NAME="$INSTALLING_APP_NAME"
   else
     _show_error_message "Couldn't find bash-completion directory!"
     echo "Please select bash-completion directory manually"
     _manual_select_completion_dir || return 1
   fi
-  DATA_DIR="/usr/share/vuh"
+  DATA_DIR="/usr/share/$INSTALLING_APP_NAME"
   _install || return 1
 }
 
