6801193a837a0d3eeabb05b0bc7f2bc784d71c3c
Shishkin Sergey <shishkinsergeydmitrievich@gmail.com>
2024-06-23 22:41:03 +0300

    feat: autocomplite for project-modules command

--- vuh-completion.sh
+++ vuh-completion.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-complete -W "--help --version --configuration --update local-version main-version suggesting-version update-version --quiet" vuh
+complete -W "--help --version --configuration --update local-version main-version suggesting-version project-modules update-version --quiet" vuh
