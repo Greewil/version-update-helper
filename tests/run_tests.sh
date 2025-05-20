0cf868ad28487561d5cef8ee327c954080c9201a
Shishkin Sergey <shishkinsergeydmitrievich@gmail.com>
2025-05-20 23:25:20 +0700

    test: additions param in assert_data.csv for running test in separate env

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,7 +4,7 @@
 
 APP_NAME='run_tests.sh'
 
-TRIMMED_HEADER='test_id,branch_name,correct_result,command'
+TRIMMED_HEADER='test_id,branch_name,correct_result,use_separate_env,command'
 ASSERT_DATA_FILE='assert_data.csv'
 
 STARTING_DIR="$(pwd)"
@@ -49,9 +49,9 @@ function _run_test() {
   cd "$repo_name" || return 1
   git checkout "test_fixture_$branch_name" || return 1
   # shellcheck disable=SC2086
-  vuh_output="$(vuh $command)" || {
+  vuh_output="$("$STARTING_DIR/../vuh.sh" $command)" || {
     _show_error_message 'vuh throw an error during execution!'
-    vuh $command
+    "$STARTING_DIR/../vuh.sh" $command
     return 1
   }
   if [ "$vuh_output" != "$correct_result" ]; then
@@ -68,14 +68,16 @@ function _run_test_in_tmp_environment() {
   branch_name=$2
   correct_result=$3
   command=$4
-  test_dir=$5
+  test_dir_name=$5
   _show_updated_message "Running test '$test_id' (using branch test_fixture_$branch_name) ..."
-  if [ "$test_dir" = '' ]; then
-    test_dir="$test_id-$(date +%s%N)"
+  if [ "$test_dir_name" = '' ]; then
+    unique_test_dir="$test_id-$(date +%s%N)"
+  else
+    unique_test_dir="$test_dir_name"
   fi
   cd "$STARTING_DIR" || exit 1
-  mkdir -p "tmp/$test_dir"
-  cd "tmp/$test_dir" || exit 1
+  mkdir -p "tmp/$unique_test_dir"
+  cd "tmp/$unique_test_dir" || exit 1
   repo_name='vuh-repo'
   git clone 'git@github.com:Greewil/version-update-helper.git' "$repo_name"
   _run_test "$test_id" "$branch_name" "$correct_result" "$command" "$repo_name" || exit 1
@@ -94,26 +96,36 @@ function run_all_tests() {
     if [ "$trimmed_line" != '' ] && [ "$trimmed_line" != "$TRIMMED_HEADER" ]; then
       echo "Line without comments: $line_without_comments"
       incorrect_line='false'
+
+      # checking line hase same columns as table header
       separator=','
       line_without_separators="${line_without_comments//$separator}"
       separators_in_line=$(((${#line_without_comments} - ${#line_without_separators}) / ${#separator}))
       header_without_separators="${TRIMMED_HEADER//$separator}"
       separators_in_header=$(((${#TRIMMED_HEADER} - ${#header_without_separators}) / ${#separator}))
       [ "$separators_in_header" != "$separators_in_line" ] && incorrect_line='true'
+
+      # parsing params
       test_id=$(echo "$line_without_comments" | cut -d',' -f1) || incorrect_line='true'
       test_id=$(echo "$test_id" | awk '{$1=$1};1') || exit 1
       branch_name=$(echo "$line_without_comments" | cut -d',' -f2) || incorrect_line='true'
       branch_name=$(echo "$branch_name" | awk '{$1=$1};1') || exit 1
       correct_result=$(echo "$line_without_comments" | cut -d',' -f3) || incorrect_line='true'
       correct_result=$(echo "$correct_result" | awk '{$1=$1};1') || exit 1
-      command=$(echo "$line_without_comments" | cut -d',' -f4) || incorrect_line='true'
+      use_separate_env=$(echo "$line_without_comments" | cut -d',' -f4) || incorrect_line='true'
+      use_separate_env=$(echo "$use_separate_env" | awk '{$1=$1};1') || exit 1
+      command=$(echo "$line_without_comments" | cut -d',' -f5) || incorrect_line='true'
       command=$(echo "$command" | awk '{$1=$1};1') || exit 1
       if [ "$incorrect_line" = 'true' ]; then
         _show_error_message 'Incorrect format in line:'
         _show_error_message "'$line'"
         return 1
       fi
-      _run_test_in_tmp_environment "$test_id" "$branch_name" "$correct_result" "$command" "$test_dir"
+      current_test_dir=''
+      if [ "$use_separate_env" = 'no' ]; then
+        current_test_dir="$test_dir"
+      fi
+      _run_test_in_tmp_environment "$test_id" "$branch_name" "$correct_result" "$command" "$current_test_dir"
     fi
   done < "$ASSERT_DATA_FILE"
   _show_success_message "All test successfully finished"
@@ -124,3 +136,4 @@ run_all_tests || exit 1
 
 # TODO run only tests for specified test_id
 # TODO run only tests for specified fixture branch
+# TODO run tests in CI
